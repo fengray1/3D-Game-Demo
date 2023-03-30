a577b978cd0d68e712535b8009d194b441ef00a9
fengray1 <fengray@seas.upenn.edu>
2023-03-30 17:01:09 -0400

    ball movement

--- 3D Game Demo/Assets/Scripts/ControlPoint.cs
+++ 3D Game Demo/Assets/Scripts/ControlPoint.cs
@@ -9,16 +9,26 @@ public class ControlPoint : MonoBehaviour
     public Rigidbody ball;
 
     public float rotationSpeed = 5f;
+    public float shootPower = 20f;
+    public LineRenderer line;
 
     // Update is called once per frame
     void Update()
     {
         transform.position = ball.position;
 
-        if (Input.GetMouseButton(0)) {
+        if (Input.GetMouseButton(1)) {
             xRot += Input.GetAxis("Mouse X") * rotationSpeed;
             yRot += Input.GetAxis("Mouse Y") * rotationSpeed;
-            transform.rotation = Quaternion.Euler(yRot, xRot, 0f);
+            transform.rotation = Quaternion.Euler(-yRot, xRot, 0f);
+            line.gameObject.SetActive(true);
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, transform.position + transform.forward * 4f);
+        }
+
+        if (Input.GetMouseButtonUp(1)) {
+            ball.velocity = transform.forward * shootPower;
+            line.gameObject.SetActive(false);
         }
     }
 }
